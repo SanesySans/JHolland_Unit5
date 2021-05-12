d5efada44e4d6241beab340a50848899a94f184f
Sanesy <budbudd2010@gmail.com>
2021-05-11 17:22:17 -0700

    Finished Lesson A
    Lesson A

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private const float minForce = 10;
+    private const float maxForce = 15;
+    private const float minTorque = -10;
+    private const float maxTorque = 10;
+    private const float minXPos = -3;
+    private const float maxXPos = 3;
+    private const float ySpawnPos = -2;
+    private Rigidbody targetRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRB = GetComponent<Rigidbody>();
+
+        RandomForce();
+        RandomTorque();
+        RandomSpawnPos();
     }
 
-    // Update is called once per frame
-    void Update()
+    void RandomForce()
     {
-        
+        targetRB.AddForce(Vector3.up * Random.Range(minForce, maxForce), 
+            ForceMode.Impulse);
+    }
+
+    void RandomTorque()
+    {
+        targetRB.AddTorque(Random.Range(minTorque, maxTorque), 
+            Random.Range(minTorque, maxTorque), 
+            Random.Range(minTorque, maxTorque), ForceMode.Impulse);
+    }
+
+    void RandomSpawnPos()
+    {
+        transform.position = new Vector3(Random.Range(minXPos, maxXPos), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
